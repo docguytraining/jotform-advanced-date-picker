cc89145c9fc4845664d52ff244fdd9d04f61cace
Paul Pehrson <paul.pehrson@cyberark.com>
2025-08-07 11:21:03 -0600

    I think the Jotform portion might work now

--- widget.js
+++ widget.js
@@ -34,4 +34,19 @@ window.onload = function () {
         script.src = "advanced-date-picker.js";
         document.body.appendChild(script);
     });
+    JFCustomWidget.subscribe("ready", function(){
+        var label = JFCustomWidget.getWidgetSetting('QuestionLabel');
+        document.getElementById('calendar').innerHTML = label;
+                //subscribe to form submit event
+                JFCustomWidget.subscribe("submit", function(){
+                    var msg = {
+                        //you should valid attribute to data for JotForm
+                        //to be able to use youw widget as required
+                        valid: true,
+                        value: document.getElementById('selectedDates').value
+                    }
+                    // send value to JotForm
+                    JFCustomWidget.sendSubmit(msg);
+                });
+            });
 };
