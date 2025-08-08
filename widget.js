c1e0a0bbfef0136e169a6fb06cb8a2f21686ccd7
Paul Pehrson <paul.pehrson@gmail.com>
2025-08-08 12:33:22 -0600

    removed comment

--- widget.js
+++ widget.js
@@ -202,7 +202,7 @@
     };
   }
 
-  function onChangeHandler(selectedDates, /*dateStr, instance*/) {
+  function onChangeHandler(selectedDates) {
     // Build canonical ISO list
     state.selected = selectedDates.map(toISO);
 
